43bf77a039ae2ced2a4b95c66960c6bb4f9ec7ac
Malhar Devasthali <malhardevasthali007@gmail.com>
2021-07-02 13:22:58 +0530

    #Added Dice Rolling Animations

---
+++ Ludo/Assets/Scripts/Dice.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Threading.Tasks;
+
+public class Dice : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] diceImages;
+    private Image diceButtonImage;
+    private Animator animator;
+    private int currentDiceNumber;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        diceButtonImage = GetComponent<Image>();
+        animator = GetComponent<Animator>();
+        int rand = Random.Range(0, 6);
+        diceButtonImage.sprite = diceImages[rand];
+
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public async void ShuffleDice()
+    {
+        animator.SetBool("DiceRolling", true);
+        await Task.Delay(500);
+
+        int rand = Random.Range(0, 6);
+        diceButtonImage.sprite = diceImages[rand];
+
+
+        await Task.Delay(500);
+        rand = Random.Range(0, 6);
+        diceButtonImage.sprite = diceImages[rand];
+
+
+        await Task.Delay(500);
+        rand = Random.Range(0, 6);
+        diceButtonImage.sprite = diceImages[rand];
+
+
+        await Task.Delay(500);
+        rand = Random.Range(0, 6);
+        diceButtonImage.sprite = diceImages[rand];
+
+        animator.SetBool("DiceRolling", false);
+
+        currentDiceNumber = rand + 1;
+        Debug.Log("Dice Rolling");
+
+
+
+    }
+}
